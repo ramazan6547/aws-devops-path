7073e8b6763ec34cc8747f7bd11392eb7e369a64
Ramazan <rmzncck65@gmail.com>
2023-06-20 15:57:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer"
+import Nav from "./components/Nav"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import People from "./pages/People"
+import PersonDetail from "./pages/PersonDetail"
+import NotFound from "./pages/NotFound"
+import { Routes, Route } from "react-router-dom"
+import Paths from "./pages/Paths"
+import FullStack from "./pages/Fullstack"
+import Aws from "./pages/Aws"
+import ReactJS from "./pages/ReactJS"
+import Login from "./pages/Login"
+import PrivateRouter from "./pages/PrivateRouter"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/people/:id" element={<PersonDetail />} />
+
+        <Route path="/paths" element={<Paths />}>
+          <Route path="fullstack" element={<FullStack />}>
+            <Route path="react" element={<ReactJS />} />
+          </Route>
+          <Route path="" element={<Aws />} />
+        </Route>
+ 
+        <Route path="/contact" element={<PrivateRouter />}>
+          <Route path="" element={<Contact />} />
+        </Route>
+
+        {/* <Route path="/blog" element={<PrivateRouter />}>
+          <Route path="" element={<Blog />} />
+        </Route> */}
+
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
